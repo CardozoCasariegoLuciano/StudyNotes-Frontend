b9ad083e8dad409377b141c93a356d1d588adeeb
CardozoCasariegoLuciano <54756423+CardozoCasariegoLuciano@users.noreply.github.com>
2023-07-26 22:27:11 -0300

    SNTAB-85 fix margin bottom in login and register page (#14)

--- src/helpers/URLs.ts
+++ src/helpers/URLs.ts
@@ -1,4 +1,5 @@
-const backendHost = "http://localhost:5000";
+//const backendHost = "http://localhost:5000";
+const backendHost = "http://192.168.1.45:5000";
 const baseURL = "api/v1";
 
 export const UrlBank = {
